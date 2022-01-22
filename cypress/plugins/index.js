95e71f906354e4da5eaca1d7361496adcec094ef
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-01-22 10:12:59 -0500

    log the hint [skip ci]

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,6 +14,7 @@ module.exports = (on, config) => {
 
   on('task', {
     async sendHintEmail({ screenshot, hint }) {
+      console.log('sending hint %s', hint)
       const screenshotPath = path.join(config.screenshotsFolder, screenshot)
       console.log('screenshotPath', screenshotPath)
 
