ac1f937cbec146f88cb8755c651abc5eeb732a50
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-01-25 14:47:04 -0500

    More page objects (#4)
    * move try next word to page object
    
    * split checking

--- cypress/integration/pages.js
+++ cypress/integration/pages.js
@@ -1,7 +1,8 @@
+// @ts-check
 /// <reference types="cypress" />
 
 // common page operations: closing the modals, etc
-import { enterWord, tryNextWord } from './utils'
+import { enterWord, pickWordWithUniqueLetters } from './utils'
 
 const silent = { log: false }
 
@@ -10,6 +11,69 @@ export const Start = {
     cy.log('**hiding the initial game modal**')
     return cy.get('game-icon[icon=close]:visible').click().wait(1000, silent)
   },
+}
+
+function updateWordList(wordList, word, letters) {
+  // look at the letters by status: first the correct ones,
+  // then the present ones, then the absent ones
+  const correctLetters = Cypress._.filter(letters, {
+    evaluation: 'correct',
+  })
+  if (correctLetters.length === 5) {
+    return word // solved!
+  }
+
+  // immediately exclude the current word from the list
+  wordList = wordList.filter((w) => w !== word)
+
+  const ordered = [].concat(
+    correctLetters,
+    Cypress._.filter(letters, { evaluation: 'present' }),
+    Cypress._.filter(letters, { evaluation: 'absent' }),
+  )
+
+  console.table(ordered)
+  const seen = new Set()
+  ordered.forEach(({ k, letter, evaluation }) => {
+    // only consider the status from the characters
+    // we see for the first time in this word
+    if (seen.has(letter)) {
+      return
+    }
+    seen.add(letter)
+
+    if (evaluation === 'absent') {
+      wordList = wordList.filter((w) => !w.includes(letter))
+    } else if (evaluation === 'present') {
+      wordList = wordList
+        .filter((w) => w.includes(letter))
+        // but remove words where the letter is AT this position
+        // because then the letter would be "correct"
+        .filter((w) => w[k] !== letter)
+    } else if (evaluation === 'correct') {
+      wordList = wordList.filter((w) => w[k] === letter)
+    }
+  })
+  return wordList
+}
+
+export function tryNextWord(wordList, word) {
+  // we should be seeing the list shrink with each iteration
+  cy.log(`Word list has ${wordList.length} words`)
+  if (!word) {
+    word = pickWordWithUniqueLetters(wordList)
+  }
+  cy.log(`**${word}**`)
+  enterWord(word)
+
+  return Playing.getLetters(word).then((letters) => {
+    wordList = updateWordList(wordList, word, letters)
+    if (wordList === word) {
+      // we solved it!
+      return word
+    }
+    return tryNextWord(wordList)
+  })
 }
 
 export const Playing = {
@@ -25,6 +89,24 @@ export const Playing = {
     return cy
       .get('@wordList')
       .then((wordList) => tryNextWord(wordList, startWord))
+  },
+
+  /**
+   * Looks at the entered word row and collects the status of each letter
+   */
+  getLetters(word) {
+    return cy
+      .get(`game-row[letters=${word}]`)
+      .find('game-tile', silent)
+      .should('have.length', word.length)
+      .then(($tiles) => {
+        return $tiles.toArray().map((tile, k) => {
+          const letter = tile.getAttribute('letter')
+          const evaluation = tile.getAttribute('evaluation')
+          console.log('%d: letter %s is %s', k, letter, evaluation)
+          return { k, letter, evaluation }
+        })
+      })
   },
 }
 
