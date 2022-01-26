0c4c07997aacf509d2bc06a400abbec523edc675
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-01-26 11:05:15 -0500

    add greener wordle solution (#7)

---
+++ cypress/integration/a-greener-wordle/spec.js
@@ -0,0 +1,39 @@
+// @ts-check
+// solves the Greener Wordle version
+// https://agreenerworldle.org/
+
+/// <reference types="cypress" />
+
+import { Start, Playing } from '../utils/pages'
+import { solve } from '../utils/solver'
+
+describe(
+  'A greener Wordle',
+  { baseUrl: 'https://agreenerworldle.org/' },
+  () => {
+    it('solves it', () => {
+      // look up the word list in the JavaScript bundle
+      // served by the application
+      cy.intercept('GET', '**/main.js', (req) => {
+        req.continue((res) => {
+          // by inserting a variable assignment here
+          // we will get the reference to the list on the window object
+          // which is reachable from this test. Note that the code is
+          // terse, but has newlines
+          res.body = res.body.replace(
+            /=\s\[\s+"earth"/,
+            '=window.wordList=["earth"',
+          )
+        })
+      }).as('words')
+      cy.visit('/')
+        .its('wordList')
+        .as('wordList')
+        .its('length')
+        .then((n) => cy.log(`${n} words`))
+      Start.close()
+      cy.contains('.title', 'A GREENER WORLDLE')
+      solve('earth', Playing)
+    })
+  },
+)
---
+++ cypress/integration/utils/solver.js
@@ -0,0 +1,75 @@
+// @ts-check
+/// <reference types="cypress" />
+
+import { pickWordWithUniqueLetters } from '.'
+
+export function solve(startWord, pageObject) {
+  expect(pageObject).to.respondTo('enterWord').and.to.respondTo('getLetters')
+
+  return cy
+    .get('@wordList')
+    .then((wordList) => tryNextWord(wordList, startWord, pageObject))
+}
+
+function updateWordList(wordList, word, letters) {
+  // look at the letters by status: first the correct ones,
+  // then the present ones, then the absent ones
+  const correctLetters = Cypress._.filter(letters, {
+    evaluation: 'correct',
+  })
+  if (correctLetters.length === 5) {
+    return word // solved!
+  }
+
+  // immediately exclude the current word from the list
+  wordList = wordList.filter((w) => w !== word)
+
+  const ordered = [].concat(
+    correctLetters,
+    Cypress._.filter(letters, { evaluation: 'present' }),
+    Cypress._.filter(letters, { evaluation: 'absent' }),
+  )
+
+  console.table(ordered)
+  const seen = new Set()
+  ordered.forEach(({ k, letter, evaluation }) => {
+    // only consider the status from the characters
+    // we see for the first time in this word
+    if (seen.has(letter)) {
+      return
+    }
+    seen.add(letter)
+
+    if (evaluation === 'absent') {
+      wordList = wordList.filter((w) => !w.includes(letter))
+    } else if (evaluation === 'present') {
+      wordList = wordList
+        .filter((w) => w.includes(letter))
+        // but remove words where the letter is AT this position
+        // because then the letter would be "correct"
+        .filter((w) => w[k] !== letter)
+    } else if (evaluation === 'correct') {
+      wordList = wordList.filter((w) => w[k] === letter)
+    }
+  })
+  return wordList
+}
+
+function tryNextWord(wordList, word, pageObject) {
+  // we should be seeing the list shrink with each iteration
+  cy.log(`Word list has ${wordList.length} words`)
+  if (!word) {
+    word = pickWordWithUniqueLetters(wordList)
+  }
+  cy.log(`**${word}**`)
+  pageObject.enterWord(word)
+
+  return pageObject.getLetters(word).then((letters) => {
+    wordList = updateWordList(wordList, word, letters)
+    if (wordList === word) {
+      // we solved it!
+      return word
+    }
+    return tryNextWord(wordList, null, pageObject)
+  })
+}
