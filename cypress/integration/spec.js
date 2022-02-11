eecb5e4cf4737da4374de30a30caf6017cc4d506
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-02-11 08:27:36 -0500

    add video link [skip ci]

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -18,6 +18,10 @@ describe('Wordle', () => {
       })
 
     cy.log('**we solved it**')
+    // to catch the toast element, we could freeze
+    // the timers on the page using cy.clock command
+    // see "Freeze The Page Using cy.clock Command"
+    // https://youtu.be/2Q6L6bzqQRY
     cy.get('#game-toaster').contains('Genius').should('be.visible')
     cy.get('game-stats')
       .find('.container')
